fff41956dac3a78382766ffc274d506871d7828e
Willy91 <williamarques91@gmail.com>
2015-03-02 16:12:34 +0100

    added clienthandler class

---
+++ src/ClientHandler.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+
+public class ClientHandler implements Runnable {
+	
+	Socket socket;
+	
+	public ClientHandler (Socket s) {
+		this.socket = s;
+	}
+	
+	@Override
+	public void run() {
+		System.out.println(getMessage(socket));
+		
+	}
+	
+	private String getMessage(Socket c) {
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(c.getInputStream()));
+			return in.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+
+}
