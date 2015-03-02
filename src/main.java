1a16b824c6daf97ed5bad5d0c009e9176919f280
grosalex <alexbruneau93@gmail.com>
2015-03-02 14:02:13 +0100

    start

--- src/main.java
+++ src/main.java
@@ -1,9 +1,19 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try {
+			InetAddress adress = InetAddress.getByName("127.0.0.1");
+			Server server = new Server(1026,adress);
+			
+			server.start();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
