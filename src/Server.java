2ac82943e935500f52a865aa82c1b6c46f617c56
Willy91 <williamarques91@gmail.com>
2015-03-02 15:37:50 +0100

    fixed non stoping chile

--- src/Server.java
+++ src/Server.java
@@ -1,6 +1,4 @@
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
@@ -13,10 +11,12 @@ public class Server {
 	InetAddress inet;
 	ServerSocket server;
 	Socket client;
+	String message;
 	
 	public Server(int inPort, InetAddress inInet){
 		this.port=inPort;
 		this.inet=inInet;
+		message="";
 	}
 	
 	public void start(){
@@ -24,38 +24,28 @@ public class Server {
 		try {
 			server=new ServerSocket(port);
 			client =server.accept();
-			while(true) {
-				readMessage(client);
-				
+			while(message!=null) {
+				message = getMessage(client);
+				if(message!=null)
+					System.out.println(message);
 			}
-			//client.close();
+			client.close();
 	        
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 	}
 
-	private void close() {
-			try {
-				if (client != null)
-					client.close();	     
-						
-
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	}
-
-	private void readMessage(Socket c) {
+	private String getMessage(Socket c) {
 		try {
 			BufferedReader in = new BufferedReader(new InputStreamReader(c.getInputStream()));
-			System.out.println(in.readLine());
+			return in.readLine();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
+		return null;
 	}
 
 }
