23e171d84bcb1ab7f4b04c2c53f82fdd764ed45e
grosalex <alexbruneau93@gmail.com>
2015-03-09 15:56:08 +0100

    commit before pull

--- src/ServerChannel.java
+++ src/ServerChannel.java
@@ -1,11 +1,15 @@
 import java.net.InetAddress;
+import java.nio.channels.SocketChannel;
 
 
 public class ServerChannel {
 	private InetAddress inet;
 	private int port;
+	
 	public ServerChannel(InetAddress inet,int port){
 		this.inet=inet;
 		this.port=port;
 	}
+	
+
 }
