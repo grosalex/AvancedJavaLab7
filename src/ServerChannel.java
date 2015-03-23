79ce1aa4087f743af5be53d7b975a2202362f9f7
Willy91 <williamarques91@gmail.com>
2015-03-23 16:10:40 +0100

    working on multicast

--- src/ServerChannel.java
+++ src/ServerChannel.java
@@ -1,7 +1,11 @@
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.nio.channels.SocketChannel;
+import java.net.DatagramPacket;
 import java.net.InetSocketAddress;
+import java.net.MulticastSocket;
 import java.net.ServerSocket;
 import java.net.SocketAddress;
 import java.net.SocketOption;
@@ -11,7 +15,6 @@ import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.util.Iterator;
 import java.util.Set;
-
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 import java.nio.channels.ServerSocketChannel;
@@ -22,59 +25,60 @@ public class ServerChannel {
 	private InetAddress inet;
 	private int port;
 	private ServerSocketChannel serverChannel;
-	
+
 	public ServerChannel(InetAddress inet,int port){
 		this.inet=inet;
 		this.port=port;
 	}
 
 	public void start() throws IOException{
+		
 		serverChannel  = ServerSocketChannel.open();
-
-		serverChannel.socket().bind(new InetSocketAddress(port));
+		serverChannel.bind(new InetSocketAddress(inet, port));
+		
+		Selector selector = Selector.open();
 		serverChannel.configureBlocking(false);
-	    SocketChannel socketChannel = serverChannel.accept();
-	    Selector selector = Selector.open();
-	    
-	    SelectionKey key = serverChannel.register(selector, SelectionKey.OP_ACCEPT);
+		serverChannel.register(selector, SelectionKey.OP_ACCEPT);
+		
 		while(true){
+			System.out.println("in while true");
+			selector.select();
+			Set<SelectionKey> keys = selector.selectedKeys();
+			Iterator<SelectionKey> keyIterator = keys.iterator();
+			ByteBuffer buf = ByteBuffer.allocate(8192);
+			
+			while(keyIterator.hasNext()) {
+				System.out.println("init");
+				
+				SelectionKey key = keyIterator.next();
 
-			  int readyChannels = selector.select();
+				if(key.isAcceptable()) {
+					SocketChannel socketChannel = serverChannel.accept();
+					socketChannel.register(selector, SelectionKey.OP_READ);
 
-			  if(readyChannels == 0) continue;
 
+				} if (key.isConnectable()) {
+					// a connection was established with a remote server.
 
-			  Set<SelectionKey> selectedKeys = selector.selectedKeys();
+				} if (key.isReadable()) {
+					// a channel is ready for reading
+					System.out.println("isreadable");
 
-			  Iterator<SelectionKey> keyIterator = selectedKeys.iterator();
-
-			  while(keyIterator.hasNext()) {
-
-			    SelectionKey selectionKey = keyIterator.next();
-
-			    if(key.isAcceptable()) {
-			        ClientChannel client = new ClientChannel(port, inet);
-			        client.sendMessage();
-			        
-
-			    } else if (key.isConnectable()) {
-			        // a connection was established with a remote server.
-
-			    } else if (key.isReadable()) {
-			        // a channel is ready for reading
-				    ByteBuffer buf = ByteBuffer.allocate(128);
-				    socketChannel.read(buf);
-				    CharBuffer msg = Charset.forName("UTF-8").decode(buf);
+				    ((SocketChannel)key.channel()).read(buf);
+				    Charset charset = Charset.defaultCharset();
+				    buf.flip();
+				    CharBuffer msg = charset.decode(buf);
 				    System.out.println(msg);
+				    buf.compact();
+					
+				} if (key.isWritable()) {
+					// a channel is ready for writing
+				}
 
-			    } else if (key.isWritable()) {
-			        // a channel is ready for writing
-			    }
+				keyIterator.remove();
+			}
 
-			    keyIterator.remove();
-			  }
-
-		    //do something with socketChannel...
+			//do something with socketChannel...
 		}
 	}
 }
